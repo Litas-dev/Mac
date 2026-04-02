3210c05445dd3e0bd3a91bf20908feab53f437f5
kostas jankus <kostasjankus@MacBookPro.home>
2026-04-02 14:44:35 +0200

    Add Touch Bar quick pay

--- Sources/PersonalFinancesApp/Views/RootSplitView.swift
+++ Sources/PersonalFinancesApp/Views/RootSplitView.swift
@@ -32,6 +32,11 @@ struct RootSplitView: View {
         let hasAccounts = store.accounts.contains(where: { !$0.archived })
         return !(hasBills || hasIncome || hasAccounts)
     }
+    
+    private var touchBarNextDueBill: Bill? {
+        let eligible = store.bills.filter { !$0.hiddenUntilEdited && !$0.isSnoozedActive && !$0.isPaidFor(date: $0.nextDueDate) }
+        return eligible.min(by: { $0.nextDueDate < $1.nextDueDate })
+    }
 
     var body: some View {
         NavigationSplitView {
@@ -337,6 +342,16 @@ struct RootSplitView: View {
         }
         .onChange(of: store.selectedIncome) { _ in
             showIncomeCalendarDropdown = false
+        }
+        .touchBar {
+            if let b = touchBarNextDueBill {
+                Button("Pay: \(b.name)") {
+                    store.selectedBillID = b.id
+                    store.logPayment(for: b.id)
+                }
+            } else {
+                Text("No bills due")
+            }
         }
         .onAppear {
             if selectedSection == .overview {
